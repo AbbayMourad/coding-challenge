7fa457b69aa17c1638e6f3b378a608fa74ccc9cd
ABBAY Mourad <mouradabbay@gmail.com>
2021-10-13 23:32:01 +0100

    add ProductRepository

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+
+use App\Models\Category;
+use App\Models\Product;
+
+class ProductRepository extends Repository
+{
+    public function __construct() {
+        parent::__construct('Product');
+    }
+
+    public function create(array $models) {
+        $product = $models['product'];
+        $product->save();
+        $categories = $models['categories'];
+        foreach ($categories as $category)  $product->categories()->attach($category->id);
+        return $product;
+    }
+
+    public function getMany(array $conditions, array $options) {
+        if (isset($conditions['category'])) {
+            $query = Category::where('name', $conditions['category'])->firstOrFail()->products();
+        } else {
+            $query = Product::where($conditions)->with('categories');
+        }
+        // filter sort options
+        $orderBys = $this->filterSortOptions($options);
+        foreach ($orderBys as $field => $order) {
+            $query->orderBy($field, $order);
+        }
+        return $query->get();
+    }
+
+
+
+}
