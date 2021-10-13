73e7dbae92b6aa54265a125b2888c6d3df31c372
ABBAY Mourad <mouradabbay@gmail.com>
2021-10-13 23:38:04 +0100

    add ProductController and create product feature

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private ProductService $productService;
+
+    public function __construct(ProductService $productService) {
+        $this->productService = $productService;
+    }
+    
+    public function store(Request $request, CategoryController $categoryController) {
+        // validate
+        $request->validate($this->rules());
+
+        // handle categories creation if needed
+        $categoriesNames = $request->input("categories", []);
+        $categories = $this->toCategories($categoriesNames);
+        $request->merge(['categories' => $categories]);
+        $categories = $categoryController->store($request);
+
+        //create product
+        $input = $request->input();
+        $product = $this->productService->create(['product' => $input['product']], ['categories' => $categories]);
+        return response($product);
+    }
+
+    private function rules(): array {
+        return [
+            'product.name' => 'required|max:50|regex:/^[a-z0-9- ]*$/i',
+            'product.description' => 'nullable|max:255|regex:/^[a-z0-9- ]*$/i',
+            'product.price' => 'required|numeric|min:0',
+            'categories' => 'nullable|array'
+        ];
+    }
+
+    private function toCategories(array $categoriesNames): array
+    {
+        $result = [];
+        foreach ($categoriesNames as $categoryName) {
+            array_push($result, ['name' => $categoryName]);
+        }
+        return $result;
+    }
+}
