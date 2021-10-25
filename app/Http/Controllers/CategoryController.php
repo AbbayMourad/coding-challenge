3e2a637b8789c736bcb50c350d1dd9898e62ecf0
ABBAY Mourad <mouradabbay@gmail.com>
2021-10-25 02:26:08 +0100

    update category controller and add a form-req for post categories action

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
@@ -15,27 +16,18 @@ class CategoryController extends Controller
     }
 
     public function store(Request $request) {
-        $request->validate($this->rules());
         $categoriesData = $request->input("categories");
-        $categories = $this->categoryService->createMany($categoriesData);
-        return CategoryResource::collection($categories);
+        return $categories = $this->categoryService->createMany($categoriesData);
+//        return CategoryResource::collection($categories);
     }
 
     public function destroy(Request $request) {
         $id = $request->route('category');
-        return $this->categoryService->delete([ 'id' => $id ]);
+        return $this->categoryService->delete($id);
     }
 
     public function index(Request $request) {
-        $categories = $this->categoryService->getMany();
-        return CategoryResource::collection($categories);
-    }
-
-    private function rules(): array
-    {
-        return [
-            'categories.*.name' => 'required|max:50|regex:/^[a-z ]*$/i',
-            'categories.*.parent' => 'nullable|max:50|regex:/^[a-z ]*$/i'
-        ];
+        return $categories = $this->categoryService->getMany();
+//        return CategoryResource::collection($categories);
     }
 }
---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'categories' => 'array',
+            'categories.*.name' => 'required|max:50|regex:/^[a-z ]*$/i',
+            'categories.*.parent' => 'nullable|max:50|regex:/^[a-z ]*$/i'
+        ];
+    }
+}
