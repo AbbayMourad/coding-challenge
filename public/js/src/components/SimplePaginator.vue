d14d88a92a341b96888d7a5c3f54e62372c11184
ABBAY Mourad <mouradabbay@gmail.com>
2021-10-25 18:40:34 +0100

    front: add products pagination

--- public/js/src/components/SimplePaginator.vue
+++ public/js/src/components/SimplePaginator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="simple-paginator">
     <button
       v-for="(option, i) in options"
       :key="i"
@@ -19,11 +19,18 @@ export default {
   },
   methods: {
     onClick(option) {
-      this.$emit('pagination', { option })
+      this.$emit('pagination', option)
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.simple-paginator {
+  display: flex;
+  justify-content: center;
+}
+.simple-paginator > * {
+  margin-right: 20px;
+}
 </style>
--- public/js/src/pages/Products.vue
+++ public/js/src/pages/Products.vue
@@ -1,50 +1,88 @@
 <template>
   <div>
     <h1>Products List</h1>
-    <div>
+    <div v-if="isProductsExists">
       <products-list :products="products" />
-      <simple-paginator :options="paginatorOptions" />
+    </div>
+    <p v-else class="not-found-mess">No Product Found !</p>
+    <div class="paginator" v-show="isProductsExists">
+      <simple-paginator :options="paginatorOptions" @pagination="onPaginate" />
     </div>
   </div>
 </template>
 
 <script>
 import ProductsList from "../components/ProductsList.vue";
-import SimplePaginator from '../components/SimplePaginator.vue';
+import SimplePaginator from "../components/SimplePaginator.vue";
 export default {
   components: { ProductsList, SimplePaginator },
   data: () => ({
     products: [],
-    links: [],
+    links: {},
+    params: { page: 1 },
   }),
   computed: {
+    isProductsExists() {
+      return this.products.length > 0;
+    },
     paginatorOptions() {
-      const { links } = this
-      const options = []
+      const { links } = this;
+      const options = [];
       for (const link in links) {
-        const option = { label: link, disabled: !links[link] }
-        options.push(option)
+        const option = { label: link, disabled: !links[link] };
+        options.push(option);
       }
-      return options
+      const optionsOrder = { first: 1, prev: 2, next: 3, last: 4 };
+      options.sort((a, b) => optionsOrder[a.label] - optionsOrder[b.label]);
+      return options;
+    },
+  },
+  watch: {
+    params: {
+      handler() {
+        this.getProducts(this.params);
+      },
+      deep: true,
     },
   },
   created() {
-    this.getProducts()
+    this.getProducts({ page: 1 });
   },
   methods: {
-    async getProducts() {
+    async getProducts(params = {}) {
       try {
-        const { data: body } = await this.$axios.get("/products")
-        this.products = body.data
-        this.links = body.links
-        console.log("--get-products-succ", body)
+        const { data: body } = await this.$axios.get("/products", { params });
+        this.products = body.data;
+        this.links = body.links;
+        console.log("--get-products-succ", body);
       } catch (err) {
-        console.log("--get-products-err", err.response)
+        console.log("--get-products-err", err.response);
       }
+    },
+    onPaginate({ label, disabled }) {
+      if (disabled) return;
+      const page = this.links[label].match(/page=(\d+)/)[1];
+      this.params.page = page;
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.paginator {
+  margin-top: 20px;
+}
+.not-found-mess {
+  margin-top: 50px;
+  font-size: 1.5em;
+}
+.sort-by {
+  display: flex;
+  justify-content: end;
+  margin-right: 30px;
+}
+#search-form {
+  display: flex;
+  justify-content: start;
+}
 </style>
