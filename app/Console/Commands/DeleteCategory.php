4e010d03797009d1a7d945d8b54931c9da566be4
ABBAY Mourad <mouradabbay@gmail.com>
2021-10-27 17:39:37 +0100

    CLI: add delete category feature

---
+++ app/Console/Commands/DeleteCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\CategoryService;
+use Illuminate\Console\Command;
+
+class DeleteCategory extends Command
+{
+    protected $signature = 'category:delete {id}';
+
+    protected $description = 'Delete a category';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(CategoryService $categoryService)
+    {
+        $id = $this->argument("id");
+        $categoryService->delete($id);
+        $this->info("category deleted successfully");
+        return 0;
+    }
+}
